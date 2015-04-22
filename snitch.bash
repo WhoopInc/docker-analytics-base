a2e27e5d008fb9700b869a31efc87654e92e8c76
Nathaniel Waisbrot <code@waisbrot.net>
2015-04-21 21:30:05 -0400

    use some defaults to make snitches less complain-y

--- snitch.bash
+++ snitch.bash
@@ -1,5 +1,8 @@
-
+if [[ -z "$SNITCH_URL" ]]; then
+  echo "No SNITCH_URL set so we will not attempt to contact DMS"
+  exit 0;
+fi
 while true; do
   curl -sS $SNITCH_URL >/dev/null
-  sleep $SNITCH_SLEEP
+  sleep ${SNITCH_SLEEP:-60}
 done
